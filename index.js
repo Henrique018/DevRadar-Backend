8ec3c7cccfb8d38e4695bbfc85df14e9791a555d
Henrique Oliveira <henrique_trind@hotmail.com>
2020-01-14 23:10:08 -0300

    setting intial packages

---
+++ index.js
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+
+const express = require('express');
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log(`server started on port ${process.env.PORT}`);
+});
